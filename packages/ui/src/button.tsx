cb1aef0edef2b242d72c4843a5fb236c85ac2cb4
nick <nikolay.romanov164@gmail.com>
2025-06-24 02:04:25 +0300

    Updated @repo/ui

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -7,7 +7,7 @@ export interface ButtonProps
 
 export function Button({ children, ...other }: ButtonProps): React.JSX.Element {
   return (
-    <button type="button" {...other}>
+    <button type="button" {...other} style={{ color: 'red' }}>
       {children}
     </button>
   )
