4ba0e92186e27c33ea80a97be7a13040536ee252
nick <nikolay.romanov164@gmail.com>
2025-06-24 01:14:34 +0300

    Added import to button of @repo/ui

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,6 +1,8 @@
+import React from 'react'
+
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 export function Button({ children, ...other }: ButtonProps): React.JSX.Element {
@@ -8,7 +10,7 @@ export function Button({ children, ...other }: ButtonProps): React.JSX.Element {
     <button type="button" {...other}>
       {children}
     </button>
-  );
+  )
 }
 
-Button.displayName = "Button";
+Button.displayName = 'Button'
